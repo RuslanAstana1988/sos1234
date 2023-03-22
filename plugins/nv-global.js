b225ddc47870e8fb41f803c22eb458a375a44c15
rusobufon <127081191+RuslanAstana1988@users.noreply.github.com>
2023-03-22 22:20:32 +0600

    ооооо

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -17,8 +17,8 @@ let vn = './media/Водолей.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
-if (!chat.isBanned && chat.audios && m.text.match(/(Скорпион|скорпион)/gi)) {
-let vn = './media/Скорпион.mp3'
+if (!chat.isBanned && chat.audios && m.text.match(/(Расскажи о себе|расскажи о себе)/gi)) {
+let vn = './media/.mp3'немного о себе 
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
