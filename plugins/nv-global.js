2a20b227e8c0255d2327135e06a691ff993fc1f2
rusobufon <127081191+RuslanAstana1988@users.noreply.github.com>
2023-04-16 18:55:15 +0600

    оророоо

--- plugins/nv-global.js
+++ plugins/nv-global.js
@@ -87,7 +87,7 @@ let vn = './media/скучаю.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
 
-if (!chat.isBanned && chat.audios && m.text.match(/(Порно есть|порно есть|скиньте стики 18+|скиньте стики 18+)/gi)) {    
+if (!chat.isBanned && chat.audios && m.text.match(/(Скиньте стики 18+|скиньте стики 18+)/gi)) {    
 let vn = './media/лига.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
