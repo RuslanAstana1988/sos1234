95efa4b8cc7e32f29da4f1f2d6f7b72ebe99cc2b
RuslanAstana1988 <127081191+RuslanAstana1988@users.noreply.github.com>
2024-11-22 13:51:41 +0600

    сегодня
    22112024

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn, usedPrefix }) => {
 var doc = ['pdf','zip','vnd.openxmlformats-officedocument.presentationml.presentation','vnd.openxmlformats-officedocument.spreadsheetml.sheet','vnd.openxmlformats-officedocument.wordprocessingml.document']
 var document = doc[Math.floor(Math.random() * doc.length)]    
 let text = `
-*—◉ НОМЕР МОЕГО СОЗДАТЕЛЯ wa.me/+77473845569*
+*—◉ НОМЕР МОЕГО СОЗДАТЕЛЯ wa.me/+77789008940*
 `.trim()   
 let buttonMessage= {
 'document': { url: `https://github.com/BrunoSobrino/TheMystic-Bot-MD` },
@@ -28,7 +28,7 @@ let buttonMessage= {
 {buttonId: `${usedPrefix}donar`, buttonText: {displayText: 'ОПЛАТА БОТА'}, type: 1}],
 'headerType': 6 }
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-//let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Русланан 👑;;;\nFN:Руслан ан👑\nORG:Русланан👑\nTITLE:\nitem1.TEL;waid=+77473845569:+521 999 612 5657\nitem1.X-ABLabel:Русланан👑\nX-WA-BIZ-DESCRIPTION:[❗] ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴇsᴛᴇ ɴᴜᴍ ᴘᴀʀᴀ ᴄᴏsᴀs ɪᴍᴘᴏʀᴛᴀɴᴛᴇs.\nX-WA-BIZ-NAME:Русланан 👑\nEND:VCARD`
+//let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:;Русланан 👑;;;\nFN:Руслан ан👑\nORG:Русланан👑\nTITLE:\nitem1.TEL;waid=+77789008940:+521 999 612 5657\nitem1.X-ABLabel:Русланан👑\nX-WA-BIZ-DESCRIPTION:[❗] ᴄᴏɴᴛᴀᴄᴛᴀ ᴀ ᴇsᴛᴇ ɴᴜᴍ ᴘᴀʀᴀ ᴄᴏsᴀs ɪᴍᴘᴏʀᴛᴀɴᴛᴇs.\nX-WA-BIZ-NAME:Русланан 👑\nEND:VCARD`
 //await conn.sendMessage(m.chat, { contacts: { displayName: 'Русланан 👑', contacts: [{ vcard }] }}, {quoted: m})
 const data = global.owner.filter(([id, isCreator]) => id && isCreator)
 await conn.sendContact(m.chat, data.map(([id, name]) => [id, name]), m)
